20b7e5e233c007400a8aa17b6028350759a7b281
Alex <alexbenfica@gmail.com>
2018-01-19 16:55:41 -0800

    Modified parameters to run from command line

--- arguments.py
+++ arguments.py
@@ -6,20 +6,13 @@ import argparse
 # This module will parse each argument
 
 parser = argparse.ArgumentParser(
-    description="Check for broken links on many websites at once.",
+    description="Check for broken links in all pages of a website.",
     epilog="Just run and wait for the unified report.")
 
-parser.add_argument("--urls",
-    type=lambda s: unicode(s, 'utf8'),
-    help="Base urls to start checking for broken links.",                                        
-    nargs='+')
+parser.add_argument("--url", help="Base url to start checking for broken links.",nargs='+')
 
-parser.add_argument("-o","--outputDir", 
-    type=lambda s: unicode(s, 'utf8'),
-    help="Output directory to save reports.",)
+parser.add_argument("-o","--output_file", help="Output file to save report to.",)
 
-parser.add_argument("-i","--ignoreListFile", 
-    type=lambda s: unicode(s, 'utf8'),
-    help="File with urls match patterns to ignore.",)
+parser.add_argument("-i","--ignore_list", help="File with url patterns to ignore.",)
 
 args = parser.parse_args()
